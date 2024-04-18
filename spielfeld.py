2932208fe3178e0edcf7e184bd10457ea268f430
Kieran <kieranh@web.de>
2024-04-18 11:21:56 +0100

    added Feld dataclass

--- spielfeld.py
+++ spielfeld.py
@@ -1,7 +1,11 @@
+from Feld import *
+
 class Spielfeld:
     #def defines functions of class
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
+        list = []
+list.append(Feld(1))
 print("Hello there")
